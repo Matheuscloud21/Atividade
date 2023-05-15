ecd5fa08ca6bfeac512f0fa2952aa4c02e1e1ca8
MatheusCloud <Matheuscloud21@gmail.com>
2023-05-15 11:48:42 -0300

    Cabeçalho Dark Mode
    Fazendo Dark Mode

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const inputContainer = document.querySelector('input')
+
+const lightTheme = 
+
+inputContainer.addEventListener('change', function() {
+    const isChecke = inputContainer.checked
+})
