9070f10b643678f6a318151de0ab215776ad1975
MatheusCloud <Matheuscloud21@gmail.com>
2023-05-16 11:43:18 -0300

    DarkMode Feito
    dark mode feito com sucesso

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
 const inputContainer = document.querySelector('input')
+const rootElement = document.documentElement
 
-const lightTheme = 
+const lightTheme = {
+    '--background-color': '#fff',
+    '--background-label': '#111',
+    '--color': 'rgb(93, 93, 93)',
+    '--btn-inicio': '#111',
+    '--moon-sun': '#fff',
+    '--img-logo': 'invert',
+    '--transition': '1s',
+    '--box-shadow': 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset',
+
+
+}
+
+const darkTheme = {
+    '--background-color': '#111',
+    '--background-label': '#fff',
+    '--color': '#fff',
+    '--btn-inicio': '#fff',
+    '--moon-sun': '#111',
+    '--img-logo': 'invert(1)',
+    '--transition': '1s',
+    '--box-shadow' : 'rgba(255, 255, 255, 0.8) 0px 50px 100px -20px, rgba(255, 255, 255, 0.4) 0px 20px 60px -30px, rgba(255, 255, 255, 0.4) 0px -2px 6px 0px inset',
+    
+
+}
 
 inputContainer.addEventListener('change', function() {
-    const isChecke = inputContainer.checked
+    const isChecked = inputContainer.checked
+    isChecked ? changeTheme(darkTheme) : changeTheme(lightTheme)
 })
+
+
+function changeTheme(theme) {
+    for(let prop in theme){
+        changeProperty(prop, theme[prop])
+    }
+}
+
+function changeProperty(property, value) {
+    rootElement.style.setProperty(property, value)
+}
